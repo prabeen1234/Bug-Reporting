d185a768ccd4993ba407f7b7f6d05961f599011f
Rimeshsapkota <rimesh@lancemeup.com>
2024-02-21 21:52:30 +0545

    initial project setup

---
+++ bugreportingsystem/src/test/java/com/bug/reporting/system/bugreportingsystem/BugreportingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bug.reporting.system.bugreportingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugreportingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
