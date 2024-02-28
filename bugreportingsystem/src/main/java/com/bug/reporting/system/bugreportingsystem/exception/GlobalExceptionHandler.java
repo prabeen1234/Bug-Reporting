132f38f20f08e23f9de551e43125c97c4274338e
Rimeshsapkota <rimesh@lancemeup.com>
2024-02-28 23:02:03 +0545

    global exception

--- bugreportingsystem/src/main/java/com/bug/reporting/system/bugreportingsystem/exception/GlobalExceptionHandler.java
+++ bugreportingsystem/src/main/java/com/bug/reporting/system/bugreportingsystem/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.bug.reporting.system.bugreportingsystem.exception;
 
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,6 +11,61 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    /**
+     * Handle SignatureException thrown by JwtService
+     * This exception is raised when the signature can not be validated
+     * @param exception Exception object
+     * @return ResponseEntity with a message in the body
+     */
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<?> handleSignatureException(SignatureException exception){
+        CustomMessage errorResponse = new CustomMessage("Invalid JWT signature found, token is invalid");
+        return new ResponseEntity<>(errorResponse.toString(), HttpStatus.UNAUTHORIZED);
+    }
+
+    /**
+     * <p>
+     *     These method handles exceptions thrown by the controller methods
+     *     and return a ResponseEntity with a message in the body.
+     *     The message is a custom
+     * </p>
+     * @param runtimeException Exception object
+     * @return ResponseEntity with a message in the body
+     * @see ResponseEntity
+     */
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<String> handleRuntimeException(RuntimeException runtimeException){
+        return ResponseEntity.badRequest().body(runtimeException.getMessage());
+    }
+
+    /**
+     * Handle MethodArgumentNotValidException
+     * <p>
+     * This method handles MethodArgumentNotValidException thrown by the controller methods
+     * and returns a ResponseEntity with a message in the body.
+     * The message is a HashMap with field names as keys and error messages as values.
+     * </p>
+     *
+     * @param ex Exception object
+     * @return ResponseEntity with a message in the body
+     * @see ResponseEntity
+     * @see org.springframework.web.bind.MethodArgumentNotValidException
+     * @see org.springframework.web.bind.annotation.ExceptionHandler
+     */
+    @ExceptionHandler({org.springframework.web.bind.MethodArgumentNotValidException.class})
+    public ResponseEntity<?> handleMethodArgumentNotValidException(org.springframework.web.bind.MethodArgumentNotValidException ex) {
+        HashMap<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+    
+    /**
+     * <p>
+     * The message is a HashMap with field names as keys and error messages as values.
+     * </p>
+     * @param userNotFoundException Exception object
+     * @return error message
+     */
     @ExceptionHandler(UserNotFoundException.class)
     public Map<String, String> handleUserNotFound(UserNotFoundException userNotFoundException) {
         Map<String, String> entryMap = new HashMap<>();
